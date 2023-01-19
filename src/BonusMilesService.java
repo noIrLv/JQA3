f20e96990d93a0dad345fdbe8bf329424629a5d7
Михаил Новиков <spore125@gmail.com>
2023-01-19 19:10:30 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int bonus = 20;
+
+        int bonusMiles = cost / bonus;
+
+        return bonusMiles;
+
+    }
+
+
+}
